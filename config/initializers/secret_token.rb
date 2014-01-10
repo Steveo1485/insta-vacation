67985c6a68d98f16fa173c67a1e17dcea4836ffd
steveo1485 <stevennugent@gmail.com>
2014-01-10 13:06:04 -0800

    Set up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstaVacation::Application.config.secret_key_base = '08557e57fe57f37cc313c4aeb6effe69f520b608d824c13a3d8452bd5ed6c9f2bb5c62b6e2cc157eaff116ea18dd223943ea3011a6debb5fb044fb65ae582207'
