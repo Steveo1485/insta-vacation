0f3de6bd4d85f4e03dc79bd6e4271818819d0bec
steveo1485 <stevennugent@gmail.com>
2014-01-12 12:39:38 -0800

    Changed name to VacationNow to follow Instagram API terms

--- spec/features/user_can_view_map_spec.rb
+++ spec/features/user_can_view_map_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "User visits root path" do
   it "should be shown maps#index view" do
     visit(root_path)
-    expect(page).to have_content("InstaVacation")
+    expect(page).to have_content("VacationNow")
   end
 
 end
