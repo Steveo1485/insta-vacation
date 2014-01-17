e8001df2b46f544b01375ac61fbe45a173bdab07
steveo1485 <stevennugent@gmail.com>
2014-01-16 19:08:29 -0800

    Refactor map javascript, add setMarker function

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,21 @@
-function mapInitialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(39.50, -98.35),
-    zoom: 4
-  };
+var Map = {
+	initialize: function() {
+		var mapOptions = {
+		    center: new google.maps.LatLng(39.50, -98.35),
+		    zoom: 4
+		};
+		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		return map;
+	},
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
+	setMarker: function(map, latitude, longitude){
+		var myLatlng = new google.maps.LatLng(latitude, longitude);
+		var marker = new google.maps.Marker({ position: myLatlng, title:"Disneyland!" });
+		marker.setMap(map);
+	}
 }
 
-$(document).ready(mapInitialize)
+$(document).ready(function(){
+	var map = Map.initialize();
+	Map.setMarker(map, 33.81049, -117.91900);
+});
