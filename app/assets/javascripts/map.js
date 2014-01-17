fade506db8cd4013a183b7f29dac8111251d835c
steveo1485 <stevennugent@gmail.com>
2014-01-16 19:27:58 -0800

    Add setInfoWindow function to map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -12,10 +12,22 @@ var Map = {
 		var myLatlng = new google.maps.LatLng(latitude, longitude);
 		var marker = new google.maps.Marker({ position: myLatlng, title:"Disneyland!" });
 		marker.setMap(map);
+		return marker;
+	},
+
+	setInfoWindow: function(map, marker, windowContent){
+		var infowindow = new google.maps.InfoWindow({
+			content: windowContent
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
 	}
 }
 
 $(document).ready(function(){
 	var map = Map.initialize();
-	Map.setMarker(map, 33.81049, -117.91900);
+	var marker = Map.setMarker(map, 33.81049, -117.91900);
+	Map.setInfoWindow(map, marker, "<h1>Disneyland!</h1>");
 });
